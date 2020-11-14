7ddc6379689a8ec7fa4a02c2fa9ea4ff9abb12fd
JohnnytheShark <orellanajonathan7@gmail.com>
2020-11-14 13:38:15 -0500

    Added Changes

--- Folder Maker Files/Documents_FileMakerScript.ps1
+++ Folder Maker Files/Documents_FileMakerScript.ps1
@@ -1,3 +1,3 @@
 # Powershell Script to change into users documents folder then create folders 
-cd C:\users\"$env:username"\documents
-65..90 | %{mkdir ([char]$_)}
+Set-Location C:\users\"$env:username"\documents
+65..90 | ForEach-Object{mkdir ([char]$_)}
--- Organization/PictureOrganization.ps1
+++ Organization/PictureOrganization.ps1
@@ -1,10 +1,10 @@
 # Creates holding Folders, and move all files into designated folders
-"jpeg_pictures","adobe_illustrator","svg_pictures","adobe_photoshop","tiff_pictures","bitmap_pictures","gif_pictures","png_pictures","eps_pictures","raw_images" | %{mkdir $_}
+"jpeg_pictures","adobe_illustrator","svg_pictures","adobe_photoshop","tiff_pictures","bitmap_pictures","gif_pictures","png_pictures","eps_pictures","raw_images" | ForEach-Object{mkdir $_}
 Get-ChildItem "./*.jpg" | Move-Item -Destination "./jpeg_pictures"
 Get-ChildItem "./*.ai" | Move-Item -Destination "./adobe_illustrator"
 Get-ChildItem "./*.svg" | Move-Item -Destination "./svg_pictures"
 Get-ChildItem "./*.psd" | Move-Item -Destination "./adobe_photoshop"
-Get-ChildItem "./*.jpg" | Move-Item -Destination "./tiff_pictures"
+Get-ChildItem "./*.tiff" | Move-Item -Destination "./tiff_pictures"
 Get-ChildItem "./*.bmp" | Move-Item -Destination "./bitmap_pictures"
 Get-ChildItem "./*.gif" | Move-Item -Destination "./gif_pictures"
 Get-ChildItem "./*.png" | Move-Item -Destination "./png_pictures"
